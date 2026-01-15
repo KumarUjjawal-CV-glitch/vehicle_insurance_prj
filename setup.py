83c5e99327f24247131b9fd076bd9cff0d0bdadd
Ujjawal <kumarujjwal.009@gmail.com>
2026-01-16 00:15:17 +0530

    template run, installed dependencies

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ujjawal Kumar",
+    author_email="kumarujjwal.009@gmail.com",
+    packages=find_packages()
+)
