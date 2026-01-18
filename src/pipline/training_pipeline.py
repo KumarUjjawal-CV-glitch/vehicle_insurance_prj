6cbfe9ef3d079020e6cee523bf100b8bd317f714
Ujjawal <kumarujjwal.009@gmail.com>
2026-01-18 16:35:09 +0530

    added data transformation

--- src/pipline/training_pipeline.py
+++ src/pipline/training_pipeline.py
@@ -4,18 +4,22 @@ from src.logger import logging
 
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
+from src.components.data_transformation import DataTransformation
 
 from src.entity.config_entity import (DataIngestionConfig,
-                                      DataValidationConfig)
+                                      DataValidationConfig,
+                                      DataTransformationConfig)
 
 from src.entity.artifact_entity import (DataIngestionArtifact,
-                                        DataValidationArtifact)
+                                        DataValidationArtifact,
+                                        DataTransformationArtifact)
 
 
 class TranPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
+        self.data_transformation_config = DataTransformationConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
@@ -48,6 +52,20 @@ class TranPipeline:
             return data_validation_artifact
         except Exception as e:
             raise MyException(e, sys) from e
+    
+    def start_data_transformation(self,data_ingestion_artifact: DataIngestionArtifact, data_validation_artifact: DataValidationArtifact)-> DataTransformationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data transformation component
+        """
+        try:
+            data_transformation = DataTransformation(data_ingestion_artifact=data_ingestion_artifact,
+                                                     data_transformation_config=self.data_transformation_config,
+                                                     data_validation_artifact=data_validation_artifact)
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        
+        except Exception as e:
+            raise MyException(e, sys)
 
 
     def run_pipeline(self, )-> None:
@@ -57,6 +75,9 @@ class TranPipeline:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_artifact= data_validation_artifact)
 
         except Exception as e:
             raise MyException(e, sys) from e
